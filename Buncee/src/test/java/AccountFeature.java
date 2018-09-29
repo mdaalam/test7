b83bd285e7bc38b967adf2c3d152ba5edb3a6c2e
Md Alam <md.alam@footlocker.com>
2018-09-28 20:20:52 -0500

    a

---
+++ Buncee/src/test/java/AccountFeature.java
@@ -0,0 +1,50 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class AccountFeature extends SubBase {
+
+    public AccountFeature() throws IOException { }
+
+    @Test
+    public void login() throws InterruptedException {
+
+        loginToAccount();
+
+    }
+
+    @Test
+    public void logout() throws Exception {
+        loginToAccount();
+        logoutFromAccount();
+        takeTheScreenshot(new Object(){}.getClass().getEnclosingMethod().getName());
+    }
+
+
+
+//    @Test
+//    public void addAddress() throws InterruptedException {
+//
+//        loginToAccount();
+//
+//
+//
+//
+//    }
+
+
+
+//
+//    @Test
+//    public void changeName() throws InterruptedException {
+//
+//
+//    }
+
+
+
+
+
+
+}
